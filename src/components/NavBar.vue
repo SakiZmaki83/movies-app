d6d89944ade9b8ee04cb8a359407a23d5d386e76
jelena <jelena>
2018-05-15 20:40:27 +0200

    commit 2

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,8 +2,8 @@
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <router-link class="navbar-brand" to="/movies">Movies</router-link>
     <div class="navbar-nav">
-      <router-link class="nav-item nav-link" to="/contacts">Contacts</router-link>
-      <router-link class="nav-item nav-link" to="/add-contact">Add Contact</router-link>
+      <router-link class="nav-item nav-link" to="/contacts">App Movies</router-link>
+      <router-link class="nav-item nav-link" to="/add-contact">Add Movies</router-link>
     </div>
   </nav>
 </template>
